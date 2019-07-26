e124747273f3bc5c984be47b8b1325c50953e487
saiharsh976 <51949944+saiharsh976@users.noreply.github.com>
2019-07-26 19:11:02 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,10 @@
+nimu,pri=map(str,input.split())
+if(len(nimu)!=len(pri)):
+    print("no")
+else:
+    s1=[nimu.count(i) for i in nimu]
+    s2=[pri.count(i) for i in pri]
+if(s1==s2):
+    print("yes")
+else:
+    print("no")
